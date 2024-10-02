9c8c31e08c620ffb5ca8a0747917b25d04eb5e4c
yuzbamaria <54204342+yuzbamaria@users.noreply.github.com>
2024-10-02 18:46:03 +0100

    add somne thoughts on how potentially solve the challenge with negative -x, need improvement

--- katas/rotate_array.js
+++ katas/rotate_array.js
@@ -2,20 +2,32 @@ function rotateArray(arr, x) {
     if (x === 0) {
         return arr;
     };
-
-    const arrIndices= arr.length;
-    const newLength = arrIndices - x;
-    const arrNewStart = arr.slice(newLength);
-    const arrNewEnd = arr.slice(0, newLength);
-    const resultArr = arrNewStart.concat(arrNewEnd);
-    // console.log(resultArr) 
-    return resultArr;
-    // console.log(arrNewStart)
-    // console.log(arrNewEnd)
-    // console.log(arrIndices)
-    // console.log(newLength)
+    if (x > 0) {
+        const arrLength = arr.length;
+        const arrLastIndex = arrLength - x;
+        const arrNewStart = arr.slice(arrLastIndex);
+        const arrNewEnd = arr.slice(0, arrLastIndex);
+        const resultPositiveX = arrNewStart.concat(arrNewEnd);
+        // console.log(arrLength)
+        // console.log(arrLastIndex)
+        // console.log(arrNewStart)
+        // console.log(resultArr) 
+        // console.log(arrNewEnd)
+        return resultPositiveX;
+    } else {
+        const arrLastIndex = arr.length -1;
+        const startingPoint = x + arrLastIndex;
+        console.log(arrLastIndex)
+        console.log(startingPoint)
+        const arrNewStart = arr.slice(startingPoint);
+        console.log(arrNewStart)
+        const arrNewEnd = arr.slice(0, startingPoint);
+        console.log(arrNewEnd)
+        const resultNegativeX = arrNewStart.concat(arrNewEnd);
+        return resultNegativeX;
+    }
 }
 
-console.log(rotateArray([1, 2, 3, 4, 5], 3))
+console.log(rotateArray([1, 2, 3], -2))
 
 module.exports = rotateArray;
